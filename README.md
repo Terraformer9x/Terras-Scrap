6e9693eea45642da6bb7eedf3ef488ca2d379e9d
Terraformer9x <83795041+Terraformer9x@users.noreply.github.com>
2024-12-19 05:11:57 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Terra's Scrap
+A small collection of vanilla styled scrap I made for my Black Label modpack, this is them ported over using LLL.
+
+![Terra's Scrap](https://raw.githubusercontent.com/Terraformer9x/Terras-Scrap/main/screenshot.png "Terra's Scrap")
+
+# Contact
+If you have any questions or have any reports to make, you can add me on Discord: @Terraformer9x
---
+++ TerrasScrap/Plugin.cs
@@ -0,0 +1,66 @@
+﻿using BepInEx;
+using HarmonyLib;
+using LethalLevelLoader;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Unity.Netcode;
+using UnityEngine;
+
+namespace TerrasScrap;
+
+[BepInPlugin(Plugin.pluginGUID, Plugin.pluginName, Plugin.pluginVersion)]
+public class Plugin : BaseUnityPlugin
+{
+    private const string pluginGUID = "terraformer9x.TerrasScrap";
+    private const string pluginName = "Terra's Scrap";
+    private const string pluginVersion = "1.0.0";
+
+    private readonly Harmony harmony = new(pluginGUID);
+    public static Plugin Instance;
+
+    private void Awake()
+    {
+        Instance ??= this;
+
+        Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+
+        harmony.PatchAll(typeof(StartOfRoundPatch));
+    }
+
+    public static void Log(string msg) => Instance.Logger.LogInfo(msg);
+    public static void LogError(string msg) => Instance.Logger.LogError(msg);
+    public static void LogDebug(string msg) => Instance.Logger.LogDebug(msg);
+}
+
+internal class StartOfRoundPatch
+{
+    [HarmonyPatch(typeof(StartOfRound), "Awake")]
+    [HarmonyPostfix]
+    private static void StartOfRoundPostfix(ref StartOfRound __instance)
+    {
+        List<ExtendedItem> modExtendedItems = LethalLevelLoader.PatchedContent.ExtendedItems.Where(x => x.ModName == "Terra's Scrap").ToList();
+        Dictionary<string, ItemGroup> itemGroups = [];
+        Dictionary<string, Item> modItems = [];
+        foreach (ItemGroup itemGroup in Resources.FindObjectsOfTypeAll<ItemGroup>())
+        {
+            if (itemGroup != null && !itemGroups.ContainsKey(itemGroup.name) && !itemGroups.ContainsValue(itemGroup))
+            {
+                itemGroups.Add(itemGroup.name, itemGroup);
+            }
+        }
+        foreach (ExtendedItem extendedItem in modExtendedItems)
+        {
+            if (extendedItem.Item != null && !modItems.ContainsKey(extendedItem.Item.name) && !modItems.ContainsValue(extendedItem.Item))
+            {
+                modItems.Add(extendedItem.Item.name, extendedItem.Item);
+            }
+        }
+        modItems["TrafficCone"].spawnPositionTypes.Add(itemGroups["GeneralItemClass"]);
+        modItems["Tuba"].spawnPositionTypes.Add(itemGroups["GeneralItemClass"]);
+        modItems["WineBottle"].spawnPositionTypes.Add(itemGroups["TabletopItems"]);
+        modItems["Fan"].spawnPositionTypes.Add(itemGroups["GeneralItemClass"]);
+        modItems["Fan"].spawnPositionTypes.Add(itemGroups["TabletopItems"]);
+    }
+}
